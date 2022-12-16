8b31710f3930b0ab17a6052e56c61a614debff42
Sidd_ <79938155+Sidd-underscore@users.noreply.github.com>
2022-12-16 04:39:21 +0000

    lol

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,47 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
+	  <>
+    <Head>
+        <title>Roomss</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+    </Head>
+    <main className="bg-dark text-white font-poppins">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+            <span className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-white text-sm bg-dark-lighter rounded-full in-out duration-300 transition" role="alert">
+                <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">Under Development</span> <span className="text-sm font-medium">Coming soon 🎉</span>
+            </span>
+            <div className="mx-auto max-w-4xl text-5xl font-medium sm:text-7xl">
+                Collaboration
+                <span className="relative whitespace-nowrap text-primary">
+                    <svg aria-hidden="true" viewBox="0 0 418 42" className="absolute top-2/3 left-0 h-[0.58em] w-full fill-primary opacity-50" preserveAspectRatio="none">
+                        <path
+                            d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+                        ></path>
+                    </svg>
+                    <span className="relative">made free</span>
+                </span>
+                for everyone
+            </div>
+            <p className="mx-auto mt-6 max-w-2xl text-lg">
+                Most collaboration softwares are free, but set restrictions. With Roomss, however, gone are the days of 15GB storage limits, 40 minute calls or any other restriction. <br />
+                <br />
+                We offer video chats, text chats, checklists, document editing and much more.
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+            <p className="mt-10 flex justify-center gap-x-6">
+                <a href="/login" className="m-8 ease-in-out duration-150 transition hover:-translate-y-1 hover:bg-primary hover:scale-110 inline-flex justify-center items-center p-4 text-base font-medium text-white bg-dark-lighter rounded-lg">
+                    Open Roomss
+                    <svg aria-hidden="true" className="-rotate-45 ml-3 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </a>
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+    </main>
+	  </>
+  )}
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+				'dark': '#131a27',
+				'dark-low-opacity': 'rgba(19 26 39 / 70%)',
+				'dark-lighter': '#1a2332',
+				'dark-darker': '#0b1018',
+				'dark-darker-low-opacity': '#0b1018a3',        
+				'dark-darkest': '#121212',
+				'dark-lightest': '#1d2635',
+		},
+		backgroundImage: {
+				'splash-1': "url('/img/splash_1.png')",
+		},
+	},
   },
   plugins: [],
 }
