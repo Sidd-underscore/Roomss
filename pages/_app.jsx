c87ab372762f7d017f23c260833ce125eba5f43b
Sidd_ <79938155+Sidd-underscore@users.noreply.github.com>
2023-02-10 02:17:36 +0000

    vercel analytics baby

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import '../public/css/styles.css'
 import Router from 'next/router'
 import { useState, useEffect, } from 'react';
 import NProgress from 'nprogress'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
 	const [isLoading, setIsLoading] = useState(false);
@@ -47,6 +48,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Component className="bg-dark" {...pageProps} />
+			<Analytics />
 		</>
 	)
 }
