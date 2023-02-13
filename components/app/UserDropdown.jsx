76fbb0df4b1ba1c1f89c2828578598d931ea4ea9
Sidd_ <79938155+Sidd-underscore@users.noreply.github.com>
2023-02-13 00:40:13 +0000

    blurred user dropdowns to make them blend in with the surrounding area better and damn  it looks NICE

--- components/app/UserDropdown.jsx
+++ components/app/UserDropdown.jsx
@@ -9,7 +9,7 @@ const UserDropdown = ({ uid }) => {
 
 	return (
 		<div className="fixed top-0 right-0 z-50 mr-6 mt-10">
-			<Menu as="div" className="md:ring-1 md:ring-white md:ring-opacity-5 relative cursor-pointer md:p-2 mx-4/6 rounded-lg fixed md:bg-dark-darker md:backdrop-blur-xl inline-block md:focus-visible:ring-2 md:focus-visible:ring-white md:focus-visible:ring-opacity-75">
+			<Menu as="div" className="md:ring-1 md:ring-black md:ring-opacity-5 relative cursor-pointer md:p-2 mx-4/6 rounded-lg fixed md:bg-black md:bg-opacity-25 md:backdrop-blur-md inline-block md:focus-visible:ring-2 md:focus-visible:ring-white md:focus-visible:ring-opacity-75">
 				<div>
 					<Menu.Button className="inline-block w-full justify-center px-4 py-2 text-sm font-medium text-white vertical-middle focus:outline-none">
 						<AvatarDropdownPlaceholder />
@@ -18,11 +18,11 @@ const UserDropdown = ({ uid }) => {
 				<MainTransition
 					as={Fragment}
 				>
-					<Menu.Items className="absolute text-white right-0 mt-4 w-auto md:w-4/6 origin-top-right p-2 rounded-md bg-dark-darker backdrop-blur-xl shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none">
+					<Menu.Items className="absolute text-white right-0 mt-4 w-auto md:w-4/6 origin-top-right rounded-md bg-black bg-opacity-25 backdrop-blur-md shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none">
 						<Menu.Item>
 							<button
 								onClick={() => setShowStatusModal(true)}
-								className={`transition ease-in-out duration-150 hover:bg-dark-lighter group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+								className={`transition ease-in-out duration-300 hover:bg-black hover:bg-opacity-20 flex w-full items-center rounded-md rounded-b-none p-3 text-sm`}
 							>
 								Status
 
@@ -30,7 +30,7 @@ const UserDropdown = ({ uid }) => {
 						</Menu.Item>
 						<Menu.Item>
 							<button
-								className={`transition ease-in-out duration-150 hover:bg-red group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+								className={`transition ease-in-out duration-300 hover:bg-red  flex w-full items-center rounded-md rounded-t-none p-3 text-sm`}
 							>
 								Logout
 							</button>
